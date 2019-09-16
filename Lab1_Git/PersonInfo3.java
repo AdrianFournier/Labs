ea0f0fe026952812183a2ce44bffdf716dd4d447
AdrianFournier <afour046@uottawa.ca>
2019-09-16 19:50:29 -0400

    Changement au PersonInfo3.java

--- Lab1_Git/PersonInfo3.java
+++ Lab1_Git/PersonInfo3.java
@@ -1,9 +1,9 @@
 public class PersonInfo3 {
 
-    public static String firstName = "Liz";
-    public static String lastName = "Grégoire";
-    public static int studentNo = 173819;
-    public static String description = "Likes long walks along the beach";
+    public static String firstName = "Adrian";
+    public static String lastName = "Fournier";
+    public static int studentNo = 300062441;
+    public static String description = "Likes long runs along the beach";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
